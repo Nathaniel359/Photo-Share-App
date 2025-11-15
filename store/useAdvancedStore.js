55c3b36eb1e0a787fcfcd17013dba4423563ecb5
Nathaniel <njt359@gmail.com>
2025-11-15 17:10:25 -0600

    removed comment

--- store/useAdvancedStore.js
+++ store/useAdvancedStore.js
@@ -7,7 +7,7 @@ const useAdvancedStore = create(
       advancedMode: false,
       setAdvancedMode: (value) => set({ advancedMode: value }),
     }),
-    { name: "advancedMode" }  // localStorage key
+    { name: "advancedMode" } 
   )
 );
 
